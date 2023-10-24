922fceb3cf97d80c887b2eb874081a59784115ff
Kai Zhang <kai.zhang@outlook.com.au>
2023-10-24 12:29:39 +1100

    mvp backend express server

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    "mongodb+srv://root:kW1X1fYzjX1j8E1G@books-store-mern.61sdjwc.mongodb.net/books-collection?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,39 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for CORS policy
+// allow all browsers with default of cors (*)
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
+
+app.get("/", (request, response) => {
+    console.log(request);
+    return response.status(234).send("Welcome to MERN stack tutorial");
 });
+
+app.use("/books", booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
