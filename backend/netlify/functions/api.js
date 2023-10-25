c555b0b70982d5c4ba5aef7c08323622ecead187
Kai Zhang <kai.zhang@outlook.com.au>
2023-10-25 11:00:48 +1100

    netlify updates

---
+++ backend/netlify/functions/api.js
@@ -0,0 +1,35 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const api = express();
+
+api.use(express.json());
+
+app.use(
+    cors({
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+);
+
+const router = Router();
+// router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/books", booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+export const handler = serverless(api);
