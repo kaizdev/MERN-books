320e20a0f4957898e5a690b54cfde33abca1355b
Kai Zhang <kai.zhang@outlook.com.au>
2023-10-25 11:23:12 +1100

    netlify updates

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -6,37 +6,39 @@ import mongoose from "mongoose";
 import router from "../../routes/booksRoute.js";
 import { mongoDBURL } from "../../config.js";
 
-const api = express();
+export async function handler(event, context) {
+    const api = express();
 
-api.use(express.json());
+    api.use(express.json());
 
-api.use(
-    cors({
-        origin: [
-            "http://localhost:3000",
-            "http://localhost:5173",
-            "https://main--celebrated-otter-b15eba.netlify.app",
-        ],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
-    })
-);
+    api.use(
+        cors({
+            origin: [
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "https://main--celebrated-otter-b15eba.netlify.app",
+            ],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            allowedHeaders: ["Content-Type"],
+        })
+    );
 
-const booksRouter = router;
-// router.get("/hello", (req, res) => res.send("Hello World!"));
+    const booksRouter = router;
+    // router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/books", booksRoute);
+    api.use("/books", booksRoute);
 
-mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log("App connected to database");
-        api.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
+    mongoose
+        .connect(mongoDBURL)
+        .then(() => {
+            console.log("App connected to database");
+            api.listen(PORT, () => {
+                console.log(`App is listening to port: ${PORT}`);
+            });
+        })
+        .catch((error) => {
+            console.log(error);
         });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
 
-export const handler = serverless(api);
+    return serverless(api)(event, context);
+}
